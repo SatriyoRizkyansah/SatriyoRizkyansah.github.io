686f328a47182d56ab5caba698729d5074362da9
Satriyo Rizkyansah <satttrioo@gmail.Com>
2024-08-27 20:50:46 +0700

    Redesign porto section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["index.html", "./node_modules/flowbite/**/*.js"],
   theme: {
     container: {
       center: true,
@@ -12,13 +12,18 @@ module.exports = {
         primary: "#79944F",
         secondary: "#64748b",
         dark: "#0f172a",
+        primary2: { 50: "#eff6ff", 100: "#dbeafe", 200: "#bfdbfe", 300: "#93c5fd", 400: "#60a5fa", 500: "#3b82f6", 600: "#2563eb", 700: "#1d4ed8", 800: "#1e40af", 900: "#1e3a8a", 950: "#172554" },
       },
       screens: {
         "2xl": "1320px",
       },
     },
   },
-  plugins: [],
+  fontFamily: {
+    body: ["Inter", "ui-sans-serif", "system-ui", "-apple-system", "system-ui", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+    sans: ["Inter", "ui-sans-serif", "system-ui", "-apple-system", "system-ui", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+    plugins: [require("flowbite/plugin")],
+  },
 };
 
 // npx tailwindcss -i ./src/input.css -o ./src/output.css --watch
